d10d9322fbd1a720bfb773b4833888435db2c236
Jeff Schwab <jeff@unbuggy.com>
2018-06-16 14:45:00 -0400

    Start C++ solution for climbing-the-leaderboard
    These all time out on test case 6, except that list.cpp passed once.

---
+++ cpp/climbing-the-leaderboard/list.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+
+constexpr std::greater<> descending;
+
+int main() {
+
+    std::istream_iterator<unsigned> in(std::cin);
+
+    std::list<unsigned> scores;
+    if (auto n = *in++) {
+        for (scores.push_back(*in++); --n;) {
+            auto score = *in++;
+            if (scores.back() != score) {
+                scores.push_back(score);
+            }
+        }
+    }
+
+    for (auto m = *in++; m--;) {
+        auto const score = *in++;
+        auto const first = begin(scores), last = end(scores);
+        auto const place = std::lower_bound(first, last, score, descending);
+        auto const index = std::distance(first, place);
+        std::cout << (index + 1) << '\n';
+        if (place == last || *place != score) {
+            scores.insert(place, score);
+        }
+    }
+}
