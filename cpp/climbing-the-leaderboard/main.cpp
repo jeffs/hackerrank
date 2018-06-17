c5f16c90457b4d312856660e5f89944532b4445a
Jeff Schwab <jeff@unbuggy.com>
2018-06-17 07:14:22 -0400

    Ugh, the slowness was iostreams

--- cpp/climbing-the-leaderboard/main.cpp
+++ cpp/climbing-the-leaderboard/main.cpp
@@ -1,121 +1,39 @@
-#include <algorithm>
-#include <array>
-#include <cassert>
-#include <cstddef>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <numeric>
-#include <vector>
+#include <algorithm>    // lower_bound
+#include <cstdio>       // printf, fputs, scanf
+#include <cstdlib>      // exit
+#include <deque>        // deque
+#include <functional>   // greater
 
-// Stores a set of unique values in descending order.  The behavior is
-// undefined if more than 1,000 distinct values are inserted.
-class flat_scoreboard {
+constexpr std::greater<> descending;
 
-    using array_type = std::array<unsigned, 1'000>;
-
-    std::size_t _size = 0;
-    array_type _values;
-
-    auto lower_bound(unsigned const value) { 
-        return std::lower_bound(begin(), end(), value, std::greater<>{});
+int read_int() {
+    int result;
+    if (std::scanf("%d", &result) != 1) {
+        std::fputs("bad input", stderr);
+        std::exit(1);
     }
-
-    // Note that this function does not maintain sort order.
-    void push_back_invalid(unsigned const value) {
-        assert(_size < capacity());
-        _values[_size++] = value;
-    };
-
-  public:
-
-    using iterator = array_type::iterator;
-    using value_type = array_type::value_type;
-
-    static constexpr std::size_t capacity() {
-        return std::tuple_size<array_type>::value;
-    }
-
-    iterator begin() { return _values.begin(); }
-    iterator end() { return begin() + _size; }
-
-    // Inserts the specified `value` into this sequence if not already present.
-    // Returns the index of the value in the sequence.
-    std::size_t insert(value_type const value) {
-        auto const bound = lower_bound(value);
-        if (bound == end()) {
-            push_back_invalid(value);
-        } else if (*bound != value) {
-            push_back_invalid(value);
-            std::rotate(bound, bound + 1, end());
-        }
-        return bound - begin();
-    }
-
-    value_type front() const {
-        assert(_size);
-        return _values.front();
-    }
-
-    std::size_t size() const { return _size; }
-};
-
-struct scoreboard {
-
-    using value_type = flat_scoreboard::value_type;
-
-  private:
-    std::vector<flat_scoreboard> _buckets;
-
-    constexpr static value_type get_rung(value_type const value) {
-        return value / flat_scoreboard::capacity();
-    }
-
-    static value_type get_rung(flat_scoreboard const& board) {
-        return board.front() / flat_scoreboard::capacity();
-    }
-
-  public:
-
-    std::size_t insert(value_type const value) {
-        auto const rung = get_rung(value);
-        auto const bound = std::lower_bound(
-                _buckets.begin(), _buckets.end(),
-                rung,
-                [](flat_scoreboard const& board, value_type const rung) {
-                    return get_rung(board) > rung;
-                });
-        auto const pos =
-            bound ==_buckets.end() ? (
-                    _buckets.emplace_back(),
-                    _buckets.end() - 1)
-          : get_rung(*bound) == rung ? bound
-          : _buckets.insert(bound, {});
-        auto const index_within_bucket = pos->insert(value);
-        auto const count_before_bucket = std::accumulate(
-                _buckets.begin(), pos,
-                std::size_t{},
-                [](std::size_t const count, flat_scoreboard const& bucket) {
-                    return count + bucket.size();
-                });
-        return count_before_bucket + index_within_bucket;
-    }
-};
+    return result;
+}
 
 int main() {
 
-    std::istream_iterator<unsigned> in(std::cin);
-
-    scoreboard scores;
-    for (auto n = *in++; n--;) {
-        auto const value = *in++;
-        scores.insert(value);
+    std::deque<int> scores;
+    if (auto n = read_int()) {
+        for (scores.push_back(read_int()); --n;) {
+            auto const score = read_int();
+            if (scores.back() != score) {
+                scores.push_back(score);
+            }
+        }
     }
 
-    std::cerr << "Logging Output\n";
-
-    for (auto m = *in++; m--;) {
-        auto const place = scores.insert(*in++) + 1;
-        std::cout << place << '\n';
+    for (auto m = read_int(); m--;) {
+        auto const score = read_int();
+        auto const first = begin(scores), last = end(scores);
+        auto const place = std::lower_bound(first, last, score, descending);
+        std::printf("%d\n", static_cast<int>(place - first + 1));
+        if (place == last || *place != score) {
+            scores.insert(place, score);
+        }
     }
 }
